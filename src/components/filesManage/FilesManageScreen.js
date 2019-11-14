bad93f3c777b1839edf881e772d04628f11be18a
zhuxiuwu <1453855454@qq.com>
2019-11-14 08:32:11 +0800

    文件管理

--- src/components/filesManage/FilesManageScreen.js
+++ src/components/filesManage/FilesManageScreen.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Text, View,StyleSheet} from 'react-native';
+import RNFS from 'react-native-fs'
 import HeaderDetailScreen from '../header/HeaderDetailScreen';
+const ExternalDirectoryPath = RNFS.ExternalDirectoryPath;
 
 export default class FilesManageScreen extends Component{
     static navigationOptions = ({ navigation }) => {
@@ -18,7 +20,7 @@ export default class FilesManageScreen extends Component{
 
     };
     componentDidMount(){
-
+        console.log(ExternalDirectoryPath)
     };
 
     onGoBaCK=()=>{
